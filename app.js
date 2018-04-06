773d7d7f7c243f7fd92acd8e924c1dbc3a144e36
Jaewon Choi <qwebnm7788@naver.com>
2018-04-07 00:47:41 +0900

    초기 설정

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var server = require('http').createServer(app);
 var fs = require('fs');
 
-var port = 3000;
+var port = 5555;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    console.log('connected by ' + req.ip);
-    res.sendFile("public/index.html", {root: __dirname});
-});
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+    console.log("CHECK");
+    res.redirect('login.html');
+}); 
+
+server.listen(port, () => {
     console.log(`Server is running on ${port} port`);
 });
