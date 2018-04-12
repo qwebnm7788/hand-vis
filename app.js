ebb137e8e3e8f8f010a1a0aba071997239b3abde
Jaewon Choi <qwebnm7788@naver.com>
2018-04-12 22:42:15 +0900

    ajax 연결 확인

--- app.js
+++ app.js
@@ -6,11 +6,33 @@ var fs = require('fs');
 var port = 5555;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+
+
+//userActions -> 현재 접속중인 사용자의 수만큼 존재
+//접속중인 사용자에게 고유한 번호를 나누어 주어 그 값으로 접근
+//현재 해당 사용자의 currentAction값을 가지고 있게 된다.
+
+var userActions = {
+    0: {
+        currentAction: 3
+    }
+};
 
 app.get('/', (req, res) => {
     res.redirect('login.html');
 }); 
+
+app.post('/recognition', (req, res) => {
+    console.log(req.body);
+    console.log(req.body.outer.value);
+    res.status(200).end();
+});
+
+app.get('/ajaxtest', (req, res) => {
+    res.jsonp(userActions[0]);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on ${port} port`);
